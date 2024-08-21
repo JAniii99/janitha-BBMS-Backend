b23149442f38bda6968ed4131bddfd55a4704e30
Janitha <janithasandaruwan97@gmail.com>
2024-08-21 09:30:10 +0530

    backend init

---
+++ src/test/java/com/example/BBMS_Backend/BbmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BBMS_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
