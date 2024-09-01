9bd3bcd5b152b1883e76e085f375494503baf689
Janitha <janithasandaruwan97@gmail.com>
2024-09-01 13:07:45 +0530

    Registeratin and login backend donne

--- src/main/java/com/example/BBMS_Backend/BbmsBackendApplication.java
+++ src/main/java/com/example/BBMS_Backend/BbmsBackendApplication.java
@@ -1,13 +1,21 @@
 package com.example.BBMS_Backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BbmsBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbmsBackendApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelmapper(){
+		return new ModelMapper();
 	}
 
 }
